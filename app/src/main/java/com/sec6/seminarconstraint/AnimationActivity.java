eaab6bab4ab677dcefa3b4bdeff724c67de4e941
fra-tran-hoang-longb <tran.hoang.longb@framgia.com>
2019-04-04 17:09:17 +0700

    Refs seminar

--- app/src/main/java/com/sec6/seminarconstraint/AnimationActivity.java
+++ app/src/main/java/com/sec6/seminarconstraint/AnimationActivity.java
@@ -8,6 +8,6 @@ public class AnimationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_animation);
+        setContentView(R.layout.activity_animation_3);
     }
 }
--- app/src/main/java/com/sec6/seminarconstraint/PlaceholderActivity.java
+++ app/src/main/java/com/sec6/seminarconstraint/PlaceholderActivity.java
@@ -1,13 +1,40 @@
 package com.sec6.seminarconstraint;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.widget.TextView;
 
-public class PlaceholderActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.Placeholder;
+
+public class PlaceholderActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Placeholder mPlaceholder;
+    ConstraintLayout mConstraintLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_placeholder);
+        TextView button_a = findViewById(R.id.a);
+        TextView button_b = findViewById(R.id.b);
+        TextView button_c = findViewById(R.id.c);
+        TextView button_d = findViewById(R.id.d);
+
+        button_a.setOnClickListener(this);
+        button_b.setOnClickListener(this);
+        button_c.setOnClickListener(this);
+        button_d.setOnClickListener(this);
+
+        mConstraintLayout = findViewById(R.id.constraint_layout);
+        mPlaceholder = findViewById(R.id.placeholder);
+    }
+
+    @Override
+    public void onClick(View v) {
+        TransitionManager.beginDelayedTransition(mConstraintLayout);
+        mPlaceholder.setContentId(v.getId());
     }
 }
